77f4c246bcad13c2719381178b8e2b158529399a
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-03-21 19:10:16 -0500

    set up project

---
+++ src/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import * as admin from 'firebase-admin';
+import credentials from './credentials.json';
+import { db } from './db';
+import { routes } from './routes';
+
+
+const uri = process.env.MONGODB_URI;
+
+admin.initializeApp({ credential: admin.credential.cert(credentials) });
+const app = express();
+
+const DB_NAME = 'ToDoList';
+
+app.use(bodyParser.json());
+
+routes.forEach(route => {
+    app[route.method](route.path, route.handler);
+});
+
+const start = async () => {
+    await db.connect(uri, DB_NAME);
+    await app.listen(8080);
+    console.log("Listening on port 8080");
+}
+
+start();
+
+process.on('exit', function() {
+    db.close();
+    console.log('Closed database!');
+});
