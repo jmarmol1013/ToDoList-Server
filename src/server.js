d66985d1736e27388e536f4a5b5922515b7b3814
juanca1013 <jmarmol1@my.centennialcollege.ca>
2023-04-18 22:09:32 -0500

    Resolve credential firebase error

--- src/server.js
+++ src/server.js
@@ -2,14 +2,25 @@ require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import * as admin from 'firebase-admin';
-import credentials from './credentials.json';
 import cors from 'cors'
 import { db } from './db';
 import { routes } from './routes';
 
 const uri = process.env.MONGODB_URI;
+const credentialsConfig = {
+  type: process.env.FIREBASE_TYPE,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  clientId: process.env.FIREBASE_CLIENT_ID,
+  authUri: process.env.FIREBASE_AUTH_URI,
+  tokenUri: process.env.FIREBASE_TOKEN_URI,
+  authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+}
 
-admin.initializeApp({ credential: admin.credential.cert(credentials) });
+admin.initializeApp({ credential: admin.credential.cert(credentialsConfig) });
 const app = express();
 
 const DB_NAME = 'ToDoList';
